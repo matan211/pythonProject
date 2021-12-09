2febdc324c39105952e6b5e2aaf1856673e5b05b
matan211 <matan21100@gmail.com>
2021-12-09 16:44:52 +0200

    my new project

--- qa5/while/targil4.6.py
+++ qa5/while/targil4.6.py
@@ -1,10 +1,12 @@
 str1 = input("enter string: ")
 char1 = input("enter char: ")
+num = 5
 i = 0
 counter = 0
 while i in range(str1.__len__()):
     if char1 == str1[i]:
-       counter += 1
+        counter += 1
     i += 1
 
 print(counter)
+print(num)
