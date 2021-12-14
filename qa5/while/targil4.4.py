c7c90e5fa5d08ea2f9a370433dd0817478635793
matan211 <matan21100@gmail.com>
2021-12-14 13:33:55 +0200

    my new project

--- qa5/while/targil4.4.py
+++ qa5/while/targil4.4.py
@@ -1,16 +1,25 @@
+# this function suggest the number
 import random
-
-guess = random.randint(0, 100)
-isEquivilant = False
+# range of random
+minimum = 0
+maximum = 100
+guess = random.randint(minimum, maximum)
+isEquivalent = False
 answer = ""
 numberOfSuggestions = 1
-while isEquivilant == False:
+# isEquivalent == False
+while not isEquivalent:
     print(f"Is the number {guess}?")
     answer = input()
     if answer == "yes":
-        isEquivilant = True
-    elif answer == "bigger" or answer == "smaller":
+        isEquivalent = True
+    elif answer == "bigger":
         numberOfSuggestions += 1
-        guess = random.randint(0, 100)
-if isEquivilant:
-    print(numberOfSuggestions)
+        minimum = max(minimum, guess)
+        guess = random.randint(minimum, maximum)
+    elif answer == "smaller":
+        numberOfSuggestions += 1
+        maximum = min(maximum, guess)
+        guess = random.randint(minimum, maximum)
+if isEquivalent:
+    print("number of suggestions is: ", numberOfSuggestions)
